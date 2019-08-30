2f85456ac8f68c08e7c2ec1d7eda296cdec719cd
kiam <kiam125@gmail.com>
2019-08-31 03:41:12 +0800

    image add can add edittext

--- app/src/main/java/ncku/edu/tw/testapplication1/MainActivity.java
+++ app/src/main/java/ncku/edu/tw/testapplication1/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    EditText editText1;
+    ImageView imageView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListAdapter listAdapter = new ListAdapter(this);
+        final ListAdapter listAdapter = new ListAdapter(this);
+        editText1 = (EditText) findViewById(R.id.editText);
+        imageView1 = (ImageView) findViewById(R.id.imageView);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
 //        GridLayoutManager manager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
         GridLayoutManager manager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false) {
             @Override
@@ -30,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
         listAdapter.addItem(new Hobby("跑步"));
         listAdapter.addItem(new Hobby("跳高"));
         listAdapter.addItem(new Hobby("跳水"));
+
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listAdapter.addItem(new Hobby(editText1.getText().toString()));
+                editText1.setText("");
+            }
+        });
     }
 }
